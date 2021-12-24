885469b59df34591c2fbe54d99fd62d7dee8243f
ywz318 <46588676+ywz318@users.noreply.github.com>
2021-12-24 11:11:04 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -11,5 +11,11 @@ sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
+#3. 签名
+sed -i "s/OpenWrt /FredYuan build $(TZ-UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#4. helloworld
+sed -i "src-git helloworld https://github.com/fw876/helloworld" openwrt/feeds.conf.default
+
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
